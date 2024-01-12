688914b40431b35f59d89c6405887c8e84174e9c
BekinTech <52346503+BekinTech@users.noreply.github.com>
2024-01-12 23:24:06 +0300

    Update main.c

--- source/main.c
+++ source/main.c
@@ -31,6 +31,110 @@
 //Sound
 #define DR_MP3_IMPLEMENTATION
 #include "dr_mp3.h"
+#include <curl/curl.h>
+#include <orbis/NetCtl.h>
+
+
+
+
+inline static void KernelPrintOut(const char* FMT, ...)
+{
+    char MessageBuf[1024];
+    va_list args;
+    va_start(args, FMT);
+    vsprintf(MessageBuf, FMT, args);
+    va_end(args);
+
+    sceKernelDebugOutText(0, MessageBuf);
+	sceKernelDebugOutText(0, "\n");
+}
+
+
+int curlDownloadFile(const char* url, const char* filename)
+{
+	OrbisNetCtlInfo proxy_info;
+	char full_url[1024];
+	CURL* curl;
+	CURLcode res;
+	FILE* fd;
+
+	curl = curl_easy_init();
+	if (!curl)
+	{
+        KernelPrintOut("ERROR: CURL INIT\n");
+		return HTTP_FAILED;
+	}
+
+	fd = fopen(filename, "wb");
+	if (!fd) {
+        KernelPrintOut("fopen Error: File path '%s'\n", filename);
+		return HTTP_FAILED;
+	}
+
+
+    KernelPrintOut("URL: %s >> %s\n", filename, url);
+
+	curl_easy_setopt(curl, CURLOPT_URL, url);
+	// Set user agent string
+	curl_easy_setopt(curl, CURLOPT_USERAGENT, HTTP_USER_AGENT);
+	// not sure how to use this when enabled
+	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+	// not sure how to use this when enabled
+	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+	// Set SSL VERSION to TLS 1.2
+	curl_easy_setopt(curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+	// Set timeout for the connection to build
+	curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 10L);
+	// Follow redirects (?)
+	curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+	// The function that will be used to write the data 
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, fwrite);
+	// The data filedescriptor which will be written to
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, fd);
+	// maximum number of redirects allowed
+	curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 20L);
+	// Fail the request if the HTTP code returned is equal to or larger than 400
+	curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);
+	// request using SSL for the FTP transfer if available
+	curl_easy_setopt(curl, CURLOPT_USE_SSL, CURLUSESSL_TRY);
+
+	// check for proxy settings
+	memset(&proxy_info, 0, sizeof(OrbisNetCtlInfo));
+	sceNetCtlGetInfo(ORBIS_NET_CTL_INFO_HTTP_PROXY_CONFIG, &proxy_info);
+
+	if (proxy_info.http_proxy_config)
+	{
+		memset(&proxy_info, 0, sizeof(OrbisNetCtlInfo));
+		sceNetCtlGetInfo(ORBIS_NET_CTL_INFO_HTTP_PROXY_SERVER, &proxy_info);
+		curl_easy_setopt(curl, CURLOPT_PROXY, proxy_info.http_proxy_server);
+		curl_easy_setopt(curl, CURLOPT_PROXYTYPE, CURLPROXY_HTTP);
+
+		memset(&proxy_info, 0, sizeof(OrbisNetCtlInfo));
+		sceNetCtlGetInfo(ORBIS_NET_CTL_INFO_HTTP_PROXY_PORT, &proxy_info);
+		curl_easy_setopt(curl, CURLOPT_PROXYPORT, proxy_info.http_proxy_port);
+	}
+
+
+	// Perform the request
+	res = curl_easy_perform(curl);
+	// close file descriptor
+	fclose(fd);
+	// cleanup
+	curl_easy_cleanup(curl);
+
+
+
+	if (res != CURLE_OK)
+	{
+        KernelPrintOut("curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+
+        remove(filename);
+		return HTTP_FAILED;
+	}
+
+	return HTTP_SUCCESS;
+}
+
 
 // Audio handle
 static int32_t audio = 0;
@@ -446,7 +550,8 @@ s32 main(s32 argc, const char* argv[])
 	// Splash screen logo (fade-in)
 	drawSplashLogo(1);
 #endif
-
+curlDownloadFile("https://download.samplelib.com/mp4/sample-30s.mp4", "/data/GoldHEN/plugins/test.mp4");
+   
 	// Load application settings
 	load_app_settings(&gcm_config);
 
